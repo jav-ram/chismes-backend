4a8c9899206daea27e18b71cedc7aa90057b9f66
ram16230 <ram16230@uvg.edu.gt>
2018-10-17 15:51:07 -0600

    udpate

--- chismes/models.py
+++ chismes/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Chismes(models.Model):
     title= models.CharField(max_length= 50)
-    content= models.CharField(max_length= 255)
+    content= models.TextField()
     date= models.DateTimeField(auto_now_add=True)
